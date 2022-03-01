4ed0b146de1ba53ba27e47970484cd048aad4006
Joss Dixon <josscdixon@gmail.com>
2022-03-01 14:28:35 +0000

    router added

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,15 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter(blog => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // }
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-  }, []);
+  const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>BLOGS LOADING</div> }
-      { blogs && <BlogList blogs={ blogs } title="The Blogs"/> }
+      { data && <BlogList blogs={ data } title="Posts"/> }
     </div>
    );
 }
